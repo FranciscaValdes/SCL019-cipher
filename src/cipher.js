9305cb48c746e15f2a250438282f524b9e46553e
FranciscaValdes <fran_valdes_m@live.com>
2021-12-07 19:05:36 -0300

    tercer commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,27 @@
-const cipher = {
-  // ...
+const cipher = {  // objeto que tiene propiedades y valores
+  
+  code : function(text, offset){ // code: propiedad del objeto cipher,donde tengo una función local, que me va a tomar parámetros.
+
+    let textOutput = "";
+    
+    for (let index= 0; index<text.length; index++) { // ciclo recorre cada caracter del texto según su índice
+     
+      let ascii = text.charCodeAt(index); // obteniendo codigo ascii de cada letra
+     
+      let asciiConverted = (ascii - 65 + offset) % 26 + 65; // obteniendo número de la nueva letra (letra codificada) pasada a ascii
+     
+      let codedText = String.fromCharCode(asciiConverted); //pasando n° ascii a letra obtenido desde ascii convertido
+     
+      textOutput+=codedText
+    }
+    return textOutput
+ }
 };
 
 export default cipher;
+ 
+ 
+
+  
+
+
--- src/index.js
+++ src/index.js
@@ -2,38 +2,39 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
-window.addEventListener("load", inicio, true);
 
-//asignándole a los eventos el código que le corresponde
-function inicio() {
-    document.getElementById("inputText").addEventListener("keyup", function (){
-        this.value = this.value.toUpperCase(); //pasando texto a mayúsculas
-    }, true);
-    document.getElementById("code").addEventListener("click", function(){
-        let text = document.getElementById("inputText").value; //obteniendo texto de entrada
-        
-        let numberDisplacement = document.getElementById("displacement").value; //obteniendo n° desplazamiento
-    
-        document.getElementById("textOutput").value = code(inputText, displacement);
-    }, true);
-        
-}
 
-function code(inputText, displacement){
-    let result = "";
-    let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+document.getElementById("code").addEventListener("click", function(){
+    let text = document.getElementById("inputText").value;
+    let offset = document.getElementById("displacement").value;
+    let cipherText = cipher.code(text,offset);
+    let finalText = document.getElementById("textOutput");
+    finalText.value = cipherText;
+}, true);
 
-    displacement = (displacement % 26 + 26) % 26; //  que dé seguro entre 0 y 25
 
-    if (inputText){
-        for (let i = 0; i<inputText.length; i++){
-            if (letters.indexOf(inputText[i]) !=-1){
-                let position = ((letters.indexOf(textInput [i]) +displacement) %26);
-                result += letters[position];
-            }
-            else
-            result += textInput [i];
-        }
-    }
-    return result;
-}
+//document.getElementById()
+
+
+// window.addEventListener("load", mayusculas, true);
+  
+// //const textOne = document.getElementById("inputText")
+// function mayusculas () {
+//     document.getElementById("inputText").addEventListener("keyup", function (){
+//         this.value = this.value.toUpperCase(); //pasando texto a mayúsculas
+//     }, true);
+// }
+// window.addEventListener("load", inicio, true);//asignándole a los eventos el código que le corresponde
+// function inicio() {
+//     document.getElementById("code").addEventListener("click", function(){
+//         let text = document.getElementById("inputText").value; //obteniendo texto de entrada
+
+//         let numberDisplacement = document.getElementById("displacement").value; //obteniendo n° desplazamiento
+
+//         document.getElementById("textOutput").value = cipher.code(stringOutput);
+//     }, true);
+
+// }
+
+
+
